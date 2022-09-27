2f8a3d8fe1feab2cddd332094e2b6064b2ed6458
JL Genon <johnlloydlibra143@gmail.com>
2022-09-27 20:22:25 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let firstName= 'Lloyd';
+
+let age = null;
+
+
+
+//log in to the console.
+function greet(name){
+
+    document.write('Hello, ' + name);  
+    document.write('\n What is on your mind?');
+    console.log(typeof(age));
+    console.log('Hello, World');
+}
+
+//math operator +, - , * , / , **, %
+let radius =10;
+const pi = 3.14;
+console.log(10/2);
+let result = pi * radius ** 2;
+document.write(result);
+
+// order of operation - B  I  D  M   A  S 
+//let partialResult = 5 * (10-3)** 2 ; 
+// document.write(partialResult);
+
+//let likes = 10 ; 
+//likes ++ ; 
+//console.log(likes);
+//likes -=5; 
+//console.log(likes);
